be95228459f227577bf8605260f329fde6d60398
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-06-10 13:14:27 +0330

    adding register,login page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+            public: `${path.resolve(__dirname, './public/')}`,
+            components: `${path.resolve(__dirname, './src/components/')}`,
+            assets: `${path.resolve(__dirname, './src/assets/')}`,
+            contexts: `${path.resolve(__dirname, './src/contexts/')}`,
+            core: `${path.resolve(__dirname, './src/core/')}`,
+            features: `${path.resolve(__dirname, './src/features/')}`,
+            layoutes: `${path.resolve(__dirname, './src/layouts/')}`,
+            pages: `${path.resolve(__dirname, './src/pages/')}`,
+        },
+    },
+    plugins: [react()],
+});
