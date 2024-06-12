7f9d8f1694e5201a52a19b63ca93fd1fd54b4178
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-06-12 13:31:58 +0330

    changeTheme added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
 import '@/core/i18n';
+import { useAppContext } from './contexts/app/app-context';
 
 const App = () => {
+    const { theme } = useAppContext();
+    useEffect(() => {
+        const head = document.head;
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = `/css/${theme}.css`;
+        head.appendChild(link);
+
+        return () => {
+            head.removeChild(link);
+        };
+    }, [theme]);
     return <RouterProvider router={router} />;
 };
 
